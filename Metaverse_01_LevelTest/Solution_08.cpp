ca766c88e096921942a335d5d614727f95a20d85
kimyounghoon <hungame@naver.com>
2022-06-13 07:58:07 +0900

    commit #2

--- Metaverse_01_LevelTest/Solution_08.cpp
+++ Metaverse_01_LevelTest/Solution_08.cpp
@@ -15,9 +15,8 @@ int main()
 	int y = 0;
 
 	int count = 1;
-	int snail_defend_x = snail;
-	int snail_defend_y = snail;
-	int turn = snail - 1;
+	int snail_defend_x = snail - 1;
+	int snail_defend_y = snail - 1;
 
 	for (int i = 0; i < snail * snail; i++)
 	{
@@ -25,27 +24,23 @@ int main()
 			int switchNum =  0;
 
 			//벽 타면 스위치 전환되게함
-			if (y == snail_defend_y - 1)
+			if (y == snail_defend_y)
 			{
-				snail_defend_y -= 1;
-				switchNum = 0;
-			}
-			else if (x == snail_defend_x - 1)
-			{
-				snail_defend_x -= 1;
 				switchNum = 1;
 			}
-			else if (y == snail_defend_y - turn && x == snail_defend_x)
+			else if (x == snail_defend_x)
 			{
-				turn--;
-				snail_defend_y -= 1;
 				switchNum = 2;
 			}
-			else if (x == snail - snail_defend_x && y == snail_defend_y - turn)
+			else if (y == snail - snail_defend_y - 1 && x == snail_defend_x)
 			{
-				turn--;
-				snail_defend_x -= 1;
+				snail_defend_y -= 1;
 				switchNum = 3;
+			}
+			else if (x == snail - snail_defend_x && y == snail - snail_defend_y - 1)
+			{
+				snail_defend_x -= 1;
+				switchNum = 0;
 			}
 
 
